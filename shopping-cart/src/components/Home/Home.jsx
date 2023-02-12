a2086fc80771cb3648fc028639aaa7f28fe3d9eb
Aayush <amaharjann01@gmail.com>
2023-02-11 16:42:38 -0800

    making two three components homeheader, homemain and footer and importing them in home

--- shopping-cart/src/components/Home/Home.jsx
+++ shopping-cart/src/components/Home/Home.jsx
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
+import HomeHeader from "./Home-header";
+import HomeMain from "./Home-main";
+import Footer from "../Footer/Footer";
+import './Home.css';
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="home-big-container">
+      <HomeHeader />
+      <HomeMain />
+      <Footer />
+    </div>
   )
 }
