1d0793395d18112c2df531f5200807f556026095
Aayush <amaharjann01@gmail.com>
2023-02-11 16:43:27 -0800

    adding home's header

---
+++ shopping-cart/src/components/Home/Home-header.jsx
@@ -0,0 +1,16 @@
+import CoverImg from "../../assets/iphone.png";
+
+export default function HomeHeader() {
+  return (
+      <header className="home-header-container">
+        <div className="home-title-container">
+          <h1>Buy the brand new Iphone 14 Pro Max.</h1>
+          <p>You can save up to 40%. Enjoy our offers.</p>
+          <button className="home-buy-button">Buy now</button>
+        </div>
+        <div className="home-image-container">
+          <img src={CoverImg} />
+        </div>
+      </header>
+  )
+}
