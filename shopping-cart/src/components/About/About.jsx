5ef33cf7cdc2830f3586f7f3247dbd00fecff10d
Aayush <amaharjann01@gmail.com>
2023-02-12 16:19:03 -0800

    adding all the necessary components

--- shopping-cart/src/components/About/About.jsx
+++ shopping-cart/src/components/About/About.jsx
@@ -1,5 +1,16 @@
+import Footer from "../Footer/Footer";
+import AboutHeading from "./About-heading";
+import AboutMain from "./About-main";
+import "./About.css";
+
 export default function About() {
   return (
-    <div>About</div>
+    <>
+      <div className="about-big-container">
+        <AboutHeading />
+        <AboutMain />
+      </div>
+      <Footer />
+    </>
   )
 }
