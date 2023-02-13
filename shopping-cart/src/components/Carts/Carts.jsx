251ca97ea11c417b93efe9ba351567be2d6cac5d
Aayush <amaharjann01@gmail.com>
2023-02-12 17:44:09 -0800

    Adding Card component also adding logic so that user can see the added item in the cart section

--- shopping-cart/src/components/Carts/Carts.jsx
+++ shopping-cart/src/components/Carts/Carts.jsx
@@ -1,5 +1,33 @@
-export default function Carts() {
+import Card from "./Cart-card.jsx";
+import { products } from "../../assets/data";
+import "../Sharedlayout/Sharedlayout.css"
+
+export default function Carts(props) {
+  let j = 0;
+  let i = 0;
+  const cartOnlyProducts = [];
+  while (j < props.cartItems.length) {
+    if (i >= products.length) {
+      i = 0;
+      j++;
+    } 
+
+    if (products[i].id == props.cartItems[j]) {
+      cartOnlyProducts.push(products[i]);
+    }
+
+    i++;
+  }
+
   return (
-    <div>Carts</div>
+    <section className="items-container">
+        {
+          cartOnlyProducts.map(product => {
+            return (
+              <Card key={product.id} data={product} />
+            )
+          })
+        }
+    </section>
   )
 }
