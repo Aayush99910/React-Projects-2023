fd2b6fc8d6794bdcd490b4006330487b1abe7ff4
Aayush <amaharjann01@gmail.com>
2023-02-12 16:18:27 -0800

    adding some handwritten data

---
+++ shopping-cart/src/components/About/about-data.js
@@ -0,0 +1,17 @@
+export const aboutDatas = [
+  {
+    title: "Customer Care",
+    description: "At our online electronic store, we prioritize customer satisfaction. Our dedicated customer care team is available to assist with any questions or concerns, and we strive for prompt and efficient service. Contact us for a seamless and enjoyable shopping experience.",
+    buttonText: "Connect with us"
+  },
+  {
+    title: "Physical Store",
+    description: "At our physical electronic store, customer service is a top priority. Our knowledgeable and friendly staff is always available to assist with any questions or concerns, and we strive to provide a personalized shopping experience. Come visit us today for a seamless and enjoyable shopping experience.",
+    buttonText: "Find the nearest store"
+  },
+  {
+    title: "Return policy",
+    description: "At our physical electronic store, customer satisfaction is a priority. Our return policy allows for returns within 30 days for a full refund or exchange, as long as the item is returned in its original condition with all accessories and materials included. Shop with confidence knowing that we stand behind our products and service.",
+    buttonText: "Read more"
+  }
+]
