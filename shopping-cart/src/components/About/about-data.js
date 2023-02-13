a03f802dc2f00f35b65d3e5626bbceeaa67c7f92
Aayush <amaharjann01@gmail.com>
2023-02-12 17:42:56 -0800

    adding id to datas

--- shopping-cart/src/components/About/about-data.js
+++ shopping-cart/src/components/About/about-data.js
@@ -1,15 +1,18 @@
 export const aboutDatas = [
   {
+    id: 0,
     title: "Customer Care",
     description: "At our online electronic store, we prioritize customer satisfaction. Our dedicated customer care team is available to assist with any questions or concerns, and we strive for prompt and efficient service. Contact us for a seamless and enjoyable shopping experience.",
     buttonText: "Connect with us"
   },
   {
+    id: 1,
     title: "Physical Store",
     description: "At our physical electronic store, customer service is a top priority. Our knowledgeable and friendly staff is always available to assist with any questions or concerns, and we strive to provide a personalized shopping experience. Come visit us today for a seamless and enjoyable shopping experience.",
     buttonText: "Find the nearest store"
   },
   {
+    id: 2,
     title: "Return policy",
     description: "At our physical electronic store, customer satisfaction is a priority. Our return policy allows for returns within 30 days for a full refund or exchange, as long as the item is returned in its original condition with all accessories and materials included. Shop with confidence knowing that we stand behind our products and service.",
     buttonText: "Read more"
