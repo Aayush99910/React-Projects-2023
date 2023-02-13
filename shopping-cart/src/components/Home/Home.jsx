3088382d99970fc72fc97da8acabddc15b098e99
Aayush <amaharjann01@gmail.com>
2023-02-12 17:46:15 -0800

    adding the addItem function to the component

--- shopping-cart/src/components/Home/Home.jsx
+++ shopping-cart/src/components/Home/Home.jsx
@@ -3,13 +3,14 @@ import HomeHeader from "./Home-header";
 import HomeMain from "./Home-main";
 import Footer from "../Footer/Footer";
 import './Home.css';
+import "../Sharedlayout/Sharedlayout.css";
 
-export default function Home() {
+export default function Home(props) {
   return (
     <>
       <div className="home-big-container">
         <HomeHeader />
-        <HomeMain />
+        <HomeMain addItems={props.addItems} />
       </div>
       <Footer />
     </>
