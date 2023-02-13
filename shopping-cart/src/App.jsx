7c129176e59d1597f5aa9a9da0ffc4b4a3f2e0a0
Aayush <amaharjann01@gmail.com>
2023-02-12 17:48:16 -0800

    adding addItems function and cartItems useState

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -5,18 +5,28 @@ import About from "./components/About/About";
 import Products from "./components/Products/Products";
 import Offers from "./components/Offers/Offers";
 import Carts from "./components/Carts/Carts";
+import { useState } from "react";
 
 export default function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  function addItems(e) {
+    const id = Number(e.target.parentElement.parentElement.id);
+    setCartItems(prevState => {
+      return [...prevState, id]
+    });
+  }
+
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SharedLayout />}>
-            <Route index element={<Home />} />
+            <Route index element={<Home addItems={addItems} />} />
             <Route path="/about" element={<About />} />
             <Route path="/products" element={<Products />} />
             <Route path="/offers" element={<Offers />} />
-            <Route path="/carts" element={<Carts />} />
+            <Route path="/carts" element={<Carts cartItems={cartItems} />} />
           </Route>
         </Routes>
       </BrowserRouter>
