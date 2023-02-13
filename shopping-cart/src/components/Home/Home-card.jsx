d0540cdd5cef47a3606d47c67256ec78ab3977f8
Aayush <amaharjann01@gmail.com>
2023-02-12 17:46:47 -0800

    Changing the css class name from home-phone to item

--- shopping-cart/src/components/Home/Home-card.jsx
+++ shopping-cart/src/components/Home/Home-card.jsx
@@ -1,16 +1,16 @@
 export default function Card(props) {
   return (
-    <article className="home-phone-container">
-      <div className="home-phone-image-container">
+    <article id={props.data.id} className="item-container">
+      <div className="item-image-container">
         <img src={props.data.imageURL} />
       </div>
-      <div className="home-phone-details-container">
+      <div className="item-details-container">
         <div>
           <h3 className="title">{props.data.name}</h3>
           <p className="price">{props.data.price}</p>
         </div>
         <p className="description">{props.data.description}</p>
-        <button className="add-to-cart-button">Add to Cart</button>
+        <button className="add-to-cart-button" onClick={props.addItems}>Add to Cart</button>
       </div>
     </article>
   )
--- shopping-cart/src/components/Home/Home-main.jsx
+++ shopping-cart/src/components/Home/Home-main.jsx
@@ -3,18 +3,18 @@ import { bestSeller } from "../../assets/data";
 import Card from "./Home-card";
 import { NavLink } from "react-router-dom";
 
-export default function HomeMain () {
+export default function HomeMain (props) {
   return (
     <main className="home-main-container">
       <div className="home-for-you">
         <h2>Phones For You!</h2>
         <NavLink to="/products">See All</NavLink>
       </div>
-      <section className="home-phones-container">
+      <section className="items-container">
         {
           products.map(product => {
             return (
-              <Card key={product.id} data={product} />
+              <Card key={product.id} data={product} addItems={props.addItems}/>
             )
           })
         }
@@ -23,7 +23,7 @@ export default function HomeMain () {
         <h2>Best Seller</h2>
         <NavLink to="/products">See All</NavLink>
       </div>
-      <section className="home-phones-container">
+      <section className="items-container">
         {
           bestSeller.map(product => {
             return (
