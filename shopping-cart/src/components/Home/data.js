58c038bda557bd8c7e406b26be87b0268fcd2c73
Aayush <amaharjann01@gmail.com>
2023-02-14 00:03:29 -0800

    moving the data from assest to here

---
+++ shopping-cart/src/components/Home/data.js
@@ -0,0 +1,148 @@
+export const phoneProducts = [
+  {
+    id: 0,
+    imageURL: "https://www.pngplay.com/wp-content/uploads/15/iPhone-14-Pro-PNG-Free-File-Download.png",
+    name: "Iphone 14 Pro",
+    price: "$999.00",
+    description: "256 gb. Smooth and efficient."
+  },
+  {
+    id: 1,
+    imageURL: "https://www.pngmart.com/files/15/Apple-iPhone-12-Background-PNG.png",
+    name: "Iphone 12",
+    price: "$899.00",
+    description: "128 gb. Vibrant color."
+  }, 
+  {
+    id: 2,
+    imageURL: "https://www.pngmart.com/files/22/Mi-PNG-Pic.png",
+    name: "Redmi 9",
+    price: "$150.00",
+    description: "32 gb. Has a lot of features."
+  },
+  {
+    id: 3,
+    imageURL: "https://www.pngmart.com/files/2/Smartphone-Transparent-Background.png",
+    name: "Iphone 8",
+    price: "$180.00",
+    description: "64gb. Built quality is superb."
+  },
+  {
+    id: 4,
+    imageURL: "https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Image.png",
+    name: "Iphone 11",
+    price: "$699.00",
+    description: "128gb. The camera is amazing."
+  },
+  {
+    id: 5,
+    imageURL: "https://www.pngmart.com/files/7/Mobile-Phone-Download-PNG-Image.png",
+    name: "Galaxy Note 9",
+    price: "$899.00",
+    description: "1TB. Smooth and fast."
+  },
+  {
+    id: 6,
+    imageURL: "https://www.pngmart.com/files/4/iPad-PNG-HD.png",
+    name: "Ipad 1",
+    price: "$80.00",
+    description: "Bigger Screen."
+  },
+  {
+    id: 7,
+    imageURL: "https://www.pngmart.com/files/22/iPhone-14-PNG-Transparent.png",
+    name: "Iphone 14",
+    price: "$899.00",
+    description: "Cheaper, smaller, efficient."
+  },
+  {
+    id: 8,
+    imageURL: "https://www.pngmart.com/files/21/iPhone-13-Pro-Max-PNG-Photos.png",
+    name: "Iphone 13 Pro Max",
+    price: "$1199.00",
+    description: "1TB. "
+  },
+  {
+    id: 9,
+    imageURL: "https://www.pngmart.com/files/7/IPhone-PNG-Background-Image.png",
+    name: "Iphone X",
+    price: "$300.00",
+    description: "Smooth with 64gb storage."
+  },
+  {
+    id: 10,
+    imageURL: "https://www.pngmart.com/files/7/Mobile-Phone-PNG-HD.png",
+    name: "Google Phone",
+    price: "$199.00",
+    description: "Matt color."
+  },
+  {
+    id: 11,
+    imageURL: "https://www.pngmart.com/files/7/Mobile-Phone-PNG-Image.png",
+    name: "Samsung 3",
+    price: "$40.00",
+    description: "Old but Gold."
+  }
+]
+
+export const bestSeller = [
+  {
+    id: 0,
+    imageURL: "https://www.pngplay.com/wp-content/uploads/15/iPhone-14-Pro-PNG-Free-File-Download.png",
+    name: "Iphone 14 Pro",
+    price: "$999.00",
+    description: "256 gb. Smooth and efficient."
+  },
+  {
+    id: 1,
+    imageURL: "https://www.pngmart.com/files/15/Apple-iPhone-12-Background-PNG.png",
+    name: "Iphone 12",
+    price: "$899.00",
+    description: "128 gb. Vibrant color."
+  }, 
+]
+
+export const airpodProducts = [
+  {
+    id: 0,
+    imageURL: "https://www.pngmart.com/files/13/Airpods-Transparent-Images-PNG.png",
+    name: "Airpods pro",
+    price: "$150",
+    description: "Original airpods Pro."
+  },
+  {
+    id: 1,
+    imageURL: "https://www.pngmart.com/files/13/Apple-Airpods-PNG-Photos.png",
+    name: "Airpods 2.0",
+    price: "$70",
+    description: "Original airpod 2.0."
+  },
+  {
+    id: 2,
+    imageURL: "https://www.pngmart.com/files/13/Airpods-Transparent-Background.png",
+    name: "Airpods in red case",
+    price: "$80",
+    description: "Red case airpods."
+  },
+  {
+    id: 3,
+    imageURL: "https://www.pngmart.com/files/13/Apple-Airpods-Transparent-PNG.png",
+    name: "Airpods in blue case.",
+    price: "$600",
+    description: "Blue case airpods"
+  },
+  {
+    id: 4,
+    imageURL: "https://www.pngmart.com/files/13/Apple-Airpods-PNG-Transparent-Image.png",
+    name: "Airpods in pink case",
+    price: "$80",
+    description: "Pink case airpods."
+  },
+  {
+    id: 5,
+    imageURL: "https://www.pngmart.com/files/13/Airpods-PNG-HD-279x279.png",
+    name: "Airpods in black case",
+    price: "$90",
+    description: "Black case airpods."
+  }
+]
