085adcfafaeb97cd88092cc584073cc028984ca8
Aayush <amaharjann01@gmail.com>
2023-02-14 23:27:52 -0800

    creating a product card

---
+++ shopping-cart/src/components/Products/Product-card.jsx
@@ -0,0 +1,17 @@
+export default function Card(props) {
+  return (
+    <article id={props.data.id} className="item-container">
+      <div className="item-image-container">
+        <img src={props.data.imageURL} />
+      </div>
+      <div className="item-details-container">
+        <div>
+          <h3 className="title">{props.data.name}</h3>
+          <p className="price">{props.data.price}</p>
+        </div>
+        <p className="description">{props.data.description}</p>
+        <button className="add-to-cart-button">Add to Cart</button>
+      </div>
+    </article>
+  )
+}
