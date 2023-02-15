ed52c54507cf1bd59e441358b88d9f0455e28e8a
Aayush <amaharjann01@gmail.com>
2023-02-14 23:27:32 -0800

    adding all products section

--- shopping-cart/src/components/Products/Products.jsx
+++ shopping-cart/src/components/Products/Products.jsx
@@ -1,7 +1,20 @@
+import { allProducts } from "../Home/data";
+import Card from "./Product-card";
+import "./Products.css"
+
 export default function Products() {
   return (
     <div className="product-big-container">
-      PRODUCTS
+      <div className="home-for-you">
+        <h2>All Products</h2>
+      </div>
+      <section className="items-container">
+        {
+          allProducts.map(product => {
+            return <Card key={product.id} data={product} />
+          })
+        }
+      </section>
     </div>
   )
 }
