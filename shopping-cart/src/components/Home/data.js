0e6b2974eb6574e3b92b77bbc9aa841480ad57e2
Aayush <amaharjann01@gmail.com>
2023-02-14 23:28:15 -0800

    adding laptop camera data

--- shopping-cart/src/components/Home/data.js
+++ shopping-cart/src/components/Home/data.js
@@ -1,87 +1,89 @@
+import { nanoid } from "nanoid";
+
 export const phoneProducts = [
   {
-    id: 0,
+    id: nanoid(),
     imageURL: "https://www.pngplay.com/wp-content/uploads/15/iPhone-14-Pro-PNG-Free-File-Download.png",
     name: "Iphone 14 Pro",
     price: "$999.00",
-    description: "256 gb. Smooth and efficient."
+    description: "256 gb. Smooth and efficient"
   },
   {
     id: 1,
     imageURL: "https://www.pngmart.com/files/15/Apple-iPhone-12-Background-PNG.png",
     name: "Iphone 12",
     price: "$899.00",
-    description: "128 gb. Vibrant color."
+    description: "128 gb. Vibrant color"
   }, 
   {
     id: 2,
     imageURL: "https://www.pngmart.com/files/22/Mi-PNG-Pic.png",
     name: "Redmi 9",
     price: "$150.00",
-    description: "32 gb. Has a lot of features."
+    description: "32 gb. Has a lot of features"
   },
   {
     id: 3,
     imageURL: "https://www.pngmart.com/files/2/Smartphone-Transparent-Background.png",
     name: "Iphone 8",
     price: "$180.00",
-    description: "64gb. Built quality is superb."
+    description: "64gb. Built quality is superb"
   },
   {
     id: 4,
     imageURL: "https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Image.png",
     name: "Iphone 11",
     price: "$699.00",
-    description: "128gb. The camera is amazing."
+    description: "128gb. The camera is amazing"
   },
   {
     id: 5,
     imageURL: "https://www.pngmart.com/files/7/Mobile-Phone-Download-PNG-Image.png",
     name: "Galaxy Note 9",
     price: "$899.00",
-    description: "1TB. Smooth and fast."
+    description: "1TB. Smooth and fast"
   },
   {
     id: 6,
     imageURL: "https://www.pngmart.com/files/4/iPad-PNG-HD.png",
     name: "Ipad 1",
     price: "$80.00",
-    description: "Bigger Screen."
+    description: "Bigger Screen"
   },
   {
     id: 7,
     imageURL: "https://www.pngmart.com/files/22/iPhone-14-PNG-Transparent.png",
     name: "Iphone 14",
     price: "$899.00",
-    description: "Cheaper, smaller, efficient."
+    description: "Cheaper, smaller, efficient"
   },
   {
     id: 8,
     imageURL: "https://www.pngmart.com/files/21/iPhone-13-Pro-Max-PNG-Photos.png",
     name: "Iphone 13 Pro Max",
     price: "$1199.00",
-    description: "1TB. "
+    description: "1TB"
   },
   {
     id: 9,
     imageURL: "https://www.pngmart.com/files/7/IPhone-PNG-Background-Image.png",
     name: "Iphone X",
     price: "$300.00",
-    description: "Smooth with 64gb storage."
+    description: "Smooth with 64gb storage"
   },
   {
     id: 10,
     imageURL: "https://www.pngmart.com/files/7/Mobile-Phone-PNG-HD.png",
     name: "Google Phone",
     price: "$199.00",
-    description: "Matt color."
+    description: "Matt color"
   },
   {
     id: 11,
     imageURL: "https://www.pngmart.com/files/7/Mobile-Phone-PNG-Image.png",
     name: "Samsung 3",
     price: "$40.00",
-    description: "Old but Gold."
+    description: "Old but Gold"
   }
 ]
 
@@ -91,14 +93,14 @@ export const bestSeller = [
     imageURL: "https://www.pngplay.com/wp-content/uploads/15/iPhone-14-Pro-PNG-Free-File-Download.png",
     name: "Iphone 14 Pro",
     price: "$999.00",
-    description: "256 gb. Smooth and efficient."
+    description: "256 gb. Smooth and efficient"
   },
   {
     id: 1,
     imageURL: "https://www.pngmart.com/files/15/Apple-iPhone-12-Background-PNG.png",
     name: "Iphone 12",
     price: "$899.00",
-    description: "128 gb. Vibrant color."
+    description: "128 gb. Vibrant color"
   }, 
 ]
 
@@ -107,42 +109,120 @@ export const airpodProducts = [
     id: 0,
     imageURL: "https://www.pngmart.com/files/13/Airpods-Transparent-Images-PNG.png",
     name: "Airpods pro",
-    price: "$150",
-    description: "Original airpods Pro."
+    price: "$150.00",
+    description: "Original airpods Pro"
   },
   {
     id: 1,
     imageURL: "https://www.pngmart.com/files/13/Apple-Airpods-PNG-Photos.png",
     name: "Airpods 2.0",
-    price: "$70",
-    description: "Original airpod 2.0."
+    price: "$70.00",
+    description: "Original airpod 2.0"
   },
   {
     id: 2,
     imageURL: "https://www.pngmart.com/files/13/Airpods-Transparent-Background.png",
     name: "Airpods in red case",
-    price: "$80",
-    description: "Red case airpods."
+    price: "$80.00",
+    description: "Red case airpods"
   },
   {
     id: 3,
     imageURL: "https://www.pngmart.com/files/13/Apple-Airpods-Transparent-PNG.png",
     name: "Airpods in blue case.",
-    price: "$600",
+    price: "$60.00",
     description: "Blue case airpods"
   },
   {
     id: 4,
     imageURL: "https://www.pngmart.com/files/13/Apple-Airpods-PNG-Transparent-Image.png",
     name: "Airpods in pink case",
-    price: "$80",
-    description: "Pink case airpods."
+    price: "$80.00",
+    description: "Pink case airpods"
   },
   {
     id: 5,
     imageURL: "https://www.pngmart.com/files/13/Airpods-PNG-HD-279x279.png",
     name: "Airpods in black case",
-    price: "$90",
-    description: "Black case airpods."
+    price: "$90.00",
+    description: "Black case airpods"
   }
 ]
+
+export const laptopProducts = [
+  {
+    id: nanoid(),
+    imageURL: "https://www.pngmart.com/files/16/Portable-Laptop-Top-View-Transparent-PNG.png",
+    name: "Laptop acer",
+    price: "$400.00",
+    description: "Small sized gaming laptop"
+  },
+  {
+    id: nanoid(),
+    imageURL: "https://www.pngmart.com/files/16/Portable-Laptop-Top-View-Transparent-Background.png",
+    name: "Laptop HP",
+    price: "$600.00",
+    description: "Touch screen hp laptop"
+  },
+  {
+    id: nanoid(),
+    imageURL: "https://www.pngmart.com/files/16/Portable-Laptop-Top-View-PNG-File.png",
+    name: "Laptop Foldable",
+    price: "$499.00",
+    description: "360 foldable laptop"
+  },
+  {
+    id: nanoid(),
+    imageURL: "https://www.pngmart.com/files/16/Laptop-Notebook-PNG-Image-363x279.png",
+    name: "Laptop i3",
+    price: "$299.00",
+    description: "Laptop i3"
+  },
+  {
+    id: nanoid(),
+    imageURL: "https://www.pngmart.com/files/7/Dell-Laptop-PNG-Transparent.png",
+    name: "DELL laptop",
+    price: "$399.00",
+    description: "Dell laptop"
+  },
+  {
+    id: nanoid(),
+    imageURL: "https://www.pngmart.com/files/1/Laptop-PNG-Free-Download.png",
+    name: "Red cover Laptop",
+    price: "$199.00",
+    description: "Red cover laptop"
+  }
+]
+
+export const cameraProducts = [
+  {
+    id: nanoid(),
+    imageURL: "https://www.pngmart.com/files/22/Nikon-PNG.png",
+    name: "Nikon 1.0",
+    price: "$600.00",
+    description: "Black nikon camera"
+  }, 
+  {
+    id: nanoid(),
+    imageURL: "https://www.pngmart.com/files/22/Nikon-PNG-Photo.png",
+    name: "Nikon 2.0",
+    price: "$589.00",
+    description: "Nikon AF-P DX"
+  },
+  {
+    id: nanoid(),
+    imageURL: "https://www.pngmart.com/files/22/Nikon-PNG-Photos.png",
+    name: "Nikon 3.0",
+    price: "$799.00",
+    description: "NIKON 24X OPTICAL ZOOM"
+  },
+  {
+    id: nanoid(),
+    imageURL: "https://www.pngmart.com/files/22/Nikon-PNG-Isolated-Image.png",
+    name: "Nikon 4.0",
+    price: "$199.00",
+    description: "NIKON 7000"
+  }
+]
+
+export const allProducts = [...phoneProducts, ...airpodProducts, ...laptopProducts, ...cameraProducts];
