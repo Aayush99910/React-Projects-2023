783cc1f51e1c32681ed5015643db2327b7d65c10
Aayush <amaharjann01@gmail.com>
2023-02-16 00:37:32 -0800

    Adding ProductHeader component

--- shopping-cart/src/components/Products/Products.jsx
+++ shopping-cart/src/components/Products/Products.jsx
@@ -1,11 +1,13 @@
 import { allProducts } from "../Home/data";
+import ProductHeader from "./Products-header";
 import Card from "./Product-card";
 import "./Products.css"
 
 export default function Products() {
   return (
     <div className="product-big-container">
-      <div className="home-for-you">
+      <ProductHeader />
+      <div>
         <h2>All Products</h2>
       </div>
       <section className="items-container">
