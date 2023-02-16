42a950c7d8d36b97477d4228a5161a14ab5fd59f
Aayush <amaharjann01@gmail.com>
2023-02-15 23:57:04 -0800

    creating home-for-youCard so that we don't have to write the same thing4 times

---
+++ shopping-cart/src/components/Home/Home-for-you-card.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+import Card from "./Home-card";
+
+const HomeForYouCard = (props) => {
+  return (
+    <div>
+			<div className="home-for-you">
+				<h2>{props.data.title}</h2>
+				<NavLink to="/products">See all</NavLink>
+			</div>
+			<section className="items-container">
+				{
+					props.data.array.map(product => {
+						return (
+							<Card key={product.id} data={product} addItems={props.addItems} />
+						)
+					})
+				}
+			</section>
+	  </div>
+  )
+}
+
+export default HomeForYouCard;
