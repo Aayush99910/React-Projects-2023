5ac20e46fd2e68dfcb8f7d306ef355bee1d1af02
Aayush <amaharjann01@gmail.com>
2023-02-19 00:14:43 -0800

    adding features for add-to-cart button

--- shopping-cart/src/components/Products/Product-card.jsx
+++ shopping-cart/src/components/Products/Product-card.jsx
@@ -10,7 +10,7 @@ export default function Card(props) {
           <p className="price">{props.data.price}</p>
         </div>
         <p className="description">{props.data.description}</p>
-        <button className="add-to-cart-button">Add to Cart</button>
+        <button className="add-to-cart-button" onClick={props.addItems}>Add to Cart</button>
       </div>
     </article>
   )
