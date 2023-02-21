971feff3516df7f696194bcf596a89a60bb46a16
Aayush <amaharjann01@gmail.com>
2023-02-20 21:52:26 -0800

    adding cart logo

--- shopping-cart/src/components/Carts/Carts.jsx
+++ shopping-cart/src/components/Carts/Carts.jsx
@@ -1,6 +1,8 @@
 import Card from "./Cart-card.jsx";
 import { allProducts } from "../Home/data.js";
 import "../Sharedlayout/Sharedlayout.css";
+import "./Carts.css";
+import cart from '../../assets/shoppingCart.png'
 
 export default function Carts(props) {
   let j = 0;
@@ -20,14 +22,34 @@ export default function Carts(props) {
   }
 
   return (
-    <section className="items-container">
-        {
-          cartOnlyProducts.map(product => {
-            return (
-              <Card key={product.id} data={product} />
-            )
-          })
-        }
-    </section>
+    <div>
+      {
+        cartOnlyProducts.length == 0 ? 
+        <div className="cart-image">
+          <div>
+            <img className="logo-image" src={cart} />
+          </div>
+          <div>Your Cart Items will appear here.</div>
+        </div> :
+        <div className="cart-big-container">
+          <div className="cart-heading">Your cart items</div>
+          <section className="cart-container height">
+            <div className="left-container">
+              {
+                cartOnlyProducts.map(product => {
+                  return (
+                    <Card key={product.id} data={product}  removeItems={props.removeItems}/>
+                  )
+                })
+              }
+            </div>
+            <div className="right-container">
+              <button className="checkout-button">Continue to Checkout</button>
+              <p>Total: ${props.total}</p>
+            </div>
+          </section> 
+        </div>
+      }
+    </div>
   )
 }
