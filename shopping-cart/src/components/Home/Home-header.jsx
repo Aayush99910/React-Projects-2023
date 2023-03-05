ed82d1175bf2ccedf3768cb5c9c08f4e09db3988
Aayush <amaharjann01@gmail.com>
2023-03-04 23:31:22 -0800

    keeping the photo on the left

--- shopping-cart/src/components/Home/Home-header.jsx
+++ shopping-cart/src/components/Home/Home-header.jsx
@@ -3,13 +3,13 @@ import CoverImg from "../../assets/iphone.png";
 export default function HomeHeader() {
   return (
       <header className="home-header-container">
+        <div className="home-image-container">
+          <img src={CoverImg} />
+        </div>
         <div className="home-title-container">
           <h1>Buy the brand new Iphone 14 Pro Max.</h1>
           <p>You can save up to 40%. Enjoy our offers.</p>
           <button className="home-buy-button">Buy now</button>
-        </div>
-        <div className="home-image-container">
-          <img src={CoverImg} />
         </div>
       </header>
   )
