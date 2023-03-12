bb36eed77f6f8e4560968e40513ef8e2e8e944db
Aayush <amaharjann01@gmail.com>
2023-03-11 23:22:20 -0800

    importing chatapp from chatapp component

--- firebase-practice/src/App.jsx
+++ firebase-practice/src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import ChatApp from "./pages/ChatApp";
 
 import "./App.css"
 
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />}/>
+        <Route path="/chatapp" element={<ChatApp />} />
       </Routes>
     </BrowserRouter>
   )
