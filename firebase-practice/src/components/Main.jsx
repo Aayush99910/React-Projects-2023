80840ad105642ac336b89bcf79da1463bee5a724
Aayush <amaharjann01@gmail.com>
2023-03-12 18:58:59 -0700

    adding redirect to homepage

--- firebase-practice/src/components/Main.jsx
+++ firebase-practice/src/components/Main.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import chatappThumbnail from "../assets/chatappThumbnail.png";
+import { Link, useNavigate } from "react-router-dom";
 
 const Main = () => {
+  const navigate = useNavigate();
+
+  //redirects the user to register page
+  const redirectUser = () => {
+    navigate("/register")
+  }
+
   return (
     <main className="main-container">
       <div className="left-container">
         <h2>
           Connect with Chatapp
         </h2>
-        <p>A simple chatapp built with the help of React and firebase.</p>
-        <button className="get-started-button">Get Started</button>
+        <p>A chatapp built with the help of React and firebase.</p>
+        <button onClick={redirectUser} className="get-started-button">Get Started</button>
       </div>
       <div className="right-container">
         <img src={chatappThumbnail}/>
